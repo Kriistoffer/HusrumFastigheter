20b590b59021ec2059f8932d0a2af57cc010e312
Kriistoffer <kjohansson93@hotmail.se>
2021-12-09 15:48:19 +0100

    Added entity

--- OOAPInlämningsuppgift2/Entities/Tenant.cs
+++ OOAPInlämningsuppgift2/Entities/Tenant.cs
@@ -2,5 +2,10 @@
 {
     public class Tenant
     {
+        public int Id { get; set; }
+        public string ApartmentNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Tag Tag { get; set; }
     }
 }
