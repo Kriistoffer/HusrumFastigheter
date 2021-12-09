52cac6863a37cecdbeeb919c7f0c332c7ecbb88f
DonOllario <oliver.stenstrom@hotmail.com>
2021-12-09 15:02:29 +0100

    Added public because i'm a dumbfuck

--- OOAPInlämningsuppgift2/Entities/Event.cs
+++ OOAPInlämningsuppgift2/Entities/Event.cs
@@ -2,7 +2,7 @@
 {
     public class Event
     {
-        string Code { get; set; }
-        string Description { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
     }
 }
