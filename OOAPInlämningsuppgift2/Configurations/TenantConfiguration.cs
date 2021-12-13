0ba3a5f9d972a269e27a36b6ae72143412bbe3b3
Kriistoffer <kjohansson93@hotmail.se>
2021-12-13 10:45:39 +0100

    wip

---
+++ OOAPInlämningsuppgift2/Configurations/TenantConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OOAPInlämningsuppgift2.Entities;
+
+namespace OOAPInlämningsuppgift2.Configurations
+{
+    public class TenantConfiguration : IEntityTypeConfiguration<Tenant>
+    {
+        public void Configure(EntityTypeBuilder<Tenant> builder)
+        {
+            builder.Property(t => t.Id)
+
+            builder.Property(t => t.ApartmentNumber)
+
+            builder.Property(t => t.FirstName)
+
+            builder.Property(t => t.LastName)
+
+            builder.Property(t => t.Tag)
+        }
+    }
+}
