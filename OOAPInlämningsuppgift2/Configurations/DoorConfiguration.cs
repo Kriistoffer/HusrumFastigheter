2884b76c117b931b6f79436eca286e546e77c918
Vivi Tran <vihanhtranjobb@hotmail.com>
2021-12-13 10:37:26 +0100

    Add door configuration

---
+++ OOAPInlämningsuppgift2/Configurations/DoorConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OOAPInlämningsuppgift2.Entities;
+
+namespace OOAPInlämningsuppgift2.Configurations
+{
+    public class DoorConfiguration : IEntityTypeConfiguration<Door>
+    {
+        public void Configure(EntityTypeBuilder<Door> builder)
+        {
+            builder
+                .HasKey(d => d.Designation);
+            builder
+                .Property(d => d.Tags)
+                .IsRequired();
+             
+        }
+    }
+}
