ed28be7e715b2b55ebc53c97b305af8aa20420e1
Kriistoffer <kjohansson93@hotmail.se>
2021-12-14 13:43:51 +0100

    Removed comments

--- OOAPInlämningsuppgift2/Entities/Tenant.cs
+++ OOAPInlämningsuppgift2/Entities/Tenant.cs
@@ -2,14 +2,12 @@
 {
     public class Tenant
     {
-        //public int Id { get; set; }
         public string ApartmentNumber { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string TagId { get; set; }
         public Tenant(string apartmentNumber, string firstName, string lastName, string tagId)
         {
-            //Id = id;
             ApartmentNumber = apartmentNumber;
             FirstName = firstName;
             LastName = lastName;
